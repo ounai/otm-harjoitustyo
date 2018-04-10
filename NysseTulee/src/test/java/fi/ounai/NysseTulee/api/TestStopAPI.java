120f3ec77e9c9559323846a9ebf9b8026c4c9a8b
Onni Rantanen <github@ounai.fi>
2018-04-10 23:56:24 +0300

    TextUI testing

---
+++ NysseTulee/src/test/java/fi/ounai/NysseTulee/api/TestStopAPI.java
@@ -0,0 +1,13 @@
+package fi.ounai.NysseTulee.api;
+
+import fi.ounai.nyssetulee.api.StopAPI;
+import fi.ounai.nyssetulee.domain.Stop;
+
+public class TestStopAPI implements StopAPI {
+
+    @Override
+    public Stop[] getStops(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- NysseTulee/src/test/java/fi/ounai/NysseTulee/domain/TextUITest.java
+++ NysseTulee/src/test/java/fi/ounai/NysseTulee/domain/TextUITest.java
@@ -1,22 +1,41 @@
 package fi.ounai.NysseTulee.domain;
 
+import fi.ounai.NysseTulee.api.TestRouteAPI;
+import fi.ounai.NysseTulee.api.TestStopAPI;
 import fi.ounai.nyssetulee.ui.TextUI;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ounai
- */
 public class TextUITest {
     
     private TextUI textUI;
     
-    public TextUITest() {
+    private String testWithInput(String input) {
+        Scanner scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+        TestRouteAPI routeAPI = new TestRouteAPI();
+        TestStopAPI stopAPI = new TestStopAPI();
         
+        TextUI textUI = new TextUI(scanner, out, routeAPI, stopAPI);
+        
+        textUI.launch();
+        
+        return new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
     }
 
+    @Test
+    public void uiWillLaunchAndTerminate() {
+        String output = testWithInput("exit");
+        assertThat(output, CoreMatchers.containsString("NysseTulee"));
+    }
 }
